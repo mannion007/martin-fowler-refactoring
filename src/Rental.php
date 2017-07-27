77f0445473b8addf49c3617adb07c7542a7e1a9a
James Mannion <mannion007@gmail.com>
2017-07-27 23:50:45 +0100

    Rename variable - thisAmount to amount

--- src/Rental.php
+++ src/Rental.php
@@ -55,26 +55,24 @@ class Rental
 
     public function getAmount()
     {
-        $thisAmount = 0;
+        $amount = 0;
         switch ($this->getMovie()->getPriceCode()) {
             case Movie::REGULAR:
-                $thisAmount += 2;
+                $amount += 2;
                 if ($this->getDaysRented() > 2) {
-                    $thisAmount += ($this->getDaysRented() - 2) * 1.5;
+                    $amount += ($this->getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie::NEW_RELEASE:
-                $thisAmount += $this->getDaysRented() * 3;
+                $amount += $this->getDaysRented() * 3;
                 break;
             case Movie::CHILDRENS:
-                $thisAmount += 1.5;
+                $amount += 1.5;
                 if ($this->getDaysRented() > 3) {
-                    $thisAmount += ($this->getDaysRented() - 3) * 1.5;
+                    $amount += ($this->getDaysRented() - 3) * 1.5;
                 }
                 break;
         }
-        return $thisAmount;
+        return $amount;
     }
-
-
 }
